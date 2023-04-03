5870bc0dfb190b6c3e1ceaa8705317964410a0be
Sahaj0312 <sahaj0091@gmail.com>
2023-04-02 17:13:08 -0700

    global variable and sma

--- analyzestock.pl
+++ analyzestock.pl
@@ -2,6 +2,18 @@
 :- use_module(library(http/http_open)).
 :- use_module(library(http/json)).
 
+:- dynamic points/0.
+
+% set the value of the global variable
+set_points(Value) :-
+    retractall(points(_)),
+    assertz(points(Value)).
+
+% get the value of the global variable
+get_points(Value) :-
+    call(points,Value).
+
+
 
 % Make an API request and get stock data
 get_stock_data(Ticker, Data) :-
@@ -30,6 +42,21 @@ parse_stock_data(Data, Ticker, Stats) :-
         change_percent: ChangePercent
     }.
 
+get_stock_sma(Ticker, Data) :-
+    api_key(APIKey),
+    format(atom(StockDataURL), 'https://www.alphavantage.co/query?function=SMA&symbol=~w&interval=weekly&time_period=10&series_type=open&apikey=~w', [Ticker, APIKey]),
+    setup_call_cleanup(
+        http_open(StockDataURL, In, []),
+        json_read_dict(In, Data),
+        close(In)
+    ).
+
+parse_stock_sma(Data, Ticker, Stats) :- 
+    get_dict('Technical Analysis: SMA', Data, Smas), 
+    get_dict('2023-03-31', Smas, SmaDict),
+    get_dict('SMA', SmaDict, SmaStr),
+    atom_number(SmaStr, Sma),
+    Stats = Sma.
 
 % Analyze the stock to determine if it's a good buy
 analyze_stock(Stats, Result) :-
@@ -42,6 +69,6 @@ analyze_stock(_, 'Not a Good Buy').
 
 
 stock_analysis(Ticker, Statistics, Result) :-
-    get_stock_data(Ticker, Data),
-    parse_stock_data(Data, Ticker, Statistics),
-    analyze_stock(Statistics, Result).
+    get_stock_sma(Ticker, Data),
+    parse_stock_sma(Data, Ticker, Statistics).
+%analyze_stock(Statistics, Result).
